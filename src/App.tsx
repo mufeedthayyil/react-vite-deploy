3e205c80934473e067aefc2328cb8a8dd9de7e9c
mufeedthayyil <mufeedthayyil6@gmail.com>
2025-06-02 20:05:09 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/Home';
+import Home from './pages/Admin';
 import Admin from './pages/Admin';
 import { AuthProvider } from './context/AuthContext';
 import { CartProvider } from './context/CartContext';
